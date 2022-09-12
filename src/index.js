44c03c62c04900533b6c17b3673574c63cdd47c0
rlal2022 <lrahul2010@gmail.com>
2022-09-12 06:17:43 -0700

    added 5 events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import './less/index.less'
+import { waitForElementToBeRemoved } from "@testing-library/dom";
+import { EvalSourceMapDevToolPlugin } from "webpack";
+import "./less/index.less";
 
 // Your code goes here!
+
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener("mouseenter", (event) => {
+  event.target.style.color = "red";
+});
+
+const blur = document.querySelector("nav a");
+blur.addEventListener("blur", (event) => {
+  blur.target.style.color = "green";
+});
+
+const dblclick = document.querySelector("footer p");
+dblclick.addEventListener("dblclick", (event) => {
+  dblclick.textContent = "You clicked me!";
+});
+
+const resize = document.querySelector(".intro h2");
+resize.addEventListener("resize", (event) => {
+  resize.textContent = "This page has been resized";
+});
+
+const wheel = document.querySelectorAll("*");
+wheel.addEventListener("wheel", (event) => {
+  wheel.classList.remove("p");
+});
